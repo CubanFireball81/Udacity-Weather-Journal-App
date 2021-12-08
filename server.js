5104f063bfde58779d00b1386725aa30d8e5460c
Alejandro Santiago <cubanfireball81@gmail.com>
2021-12-07 21:23:26 -0900

    Adding git to proejct so file history will be captured

---
+++ server.js
@@ -0,0 +1,53 @@
+// Setup empty JS object to act as endpoint for all routes
+let projectData = {
+    temp: "",
+    date: "",
+    userFeelings: ""
+};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require('body-parser');
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, () => {console.log(`running on localhost: ${port}`)});
+
+// Return all the data for the get route
+app.get("/all", (request, response) => {
+    response.send(projectData);
+});
+
+// Add all data to the project for the post route
+app.post("/add", (request, response) => {
+
+    // Throw error if temp is empty
+    if (!request.body.temp) {
+    throw new Error("Temp can not be empty!");
+    }
+
+    // Thrown error if date is empty
+    if (!request.body.date) {
+    throw new Error("Date can not be empty!");
+    }
+
+    projectData["temp"] = request.body.temp;
+    projectData["date"] = request.body.date;
+    projectData["userFeelings"] = request.body.userFeelings;
+});
---
+++ website/app.js
@@ -0,0 +1,72 @@
+/* Global Variables */
+// API URL and API key with Unit set to Imperial
+const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=d3ef38a43f231726976dc66febc96243&units=imperial';
+
+// Variable to keep temp data
+let temp = ''
+
+// Zipcode Variable
+let zipCode = '';
+
+// User input for feelings
+let feelings = '';
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+console.log(d);
+
+// Event Listener for click on Generate button
+document.getElementById('generate').addEventListener('click', doStuff);
+
+// Function called by event listener
+function doStuff (e) {
+    zipCode = document.getElementById('zip').value;
+    getAPI(weatherURL, zipCode, apiKey);
+}
+
+// Function to get Web API Data
+const getAPI = async (weatherURL, zipCode, apiKey) => {
+    const res = await fetch(weatherURL + zipCode + apiKey);
+    try {
+        const data = await res.json();
+        temp = data.main.temp;
+        console.log(temp);
+    } catch(error) {
+        console.log('error', error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Code to add to functions//
+zipCode = document.getElementById('zip').value;
+    feelings = document.getElementById('feelings').value;
+    // Update HTML element with current date for user
+    document.getElementById('date').innerHTML = d;
+    //Update HTML element with user's input for their feelings
+    document.getElementById('content').innerHTML = feelings;
+*/
